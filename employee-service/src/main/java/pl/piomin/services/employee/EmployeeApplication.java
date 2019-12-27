35d87c7fec2dae5a48c7a31817da2b2ea09cf5ff
runzexia <runzexia@yunify.com>
2019-12-27 16:11:41 +0800

    update app code
    Signed-off-by: runzexia <runzexia@yunify.com>

--- employee-service/src/main/java/pl/piomin/services/employee/EmployeeApplication.java
+++ employee-service/src/main/java/pl/piomin/services/employee/EmployeeApplication.java
@@ -5,6 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 
+import pl.piomin.services.employee.model.Employee;
+import pl.piomin.services.employee.repository.EmployeeRepository;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -25,10 +27,26 @@ public class EmployeeApplication {
 	public Docket swaggerPersonApi10() {
 		return new Docket(DocumentationType.SWAGGER_2)
 				.select()
-					.apis(RequestHandlerSelectors.basePackage("pl.piomin.services.employee.controller"))
-					.paths(PathSelectors.any())
+				.apis(RequestHandlerSelectors.basePackage("pl.piomin.services.employee.controller"))
+				.paths(PathSelectors.any())
 				.build()
 				.apiInfo(new ApiInfoBuilder().version("1.0").title("Employee API").description("Documentation Employee API v1.0").build());
 	}
-	
+
+	@Bean
+	EmployeeRepository repository() {
+		EmployeeRepository repository = new EmployeeRepository();
+		repository.add(new Employee(1L, 1L, "John Smith", 34, "Analyst"));
+		repository.add(new Employee(1L, 1L, "Darren Hamilton", 37, "Manager"));
+		repository.add(new Employee(1L, 1L, "Tom Scott", 26, "Developer"));
+		repository.add(new Employee(1L, 2L, "Anna London", 39, "Analyst"));
+		repository.add(new Employee(1L, 2L, "Patrick Dempsey", 27, "Developer"));
+		repository.add(new Employee(2L, 3L, "Kevin Price", 38, "Developer"));
+		repository.add(new Employee(2L, 3L, "Ian Scott", 34, "Developer"));
+		repository.add(new Employee(2L, 3L, "Andrew Campton", 30, "Manager"));
+		repository.add(new Employee(2L, 4L, "Steve Franklin", 25, "Developer"));
+		repository.add(new Employee(2L, 4L, "Elisabeth Smith", 30, "Developer"));
+		return repository;
+	}
+
 }
