a68d9dd95cf0852cbee21448c548acee4ef0ad7b
runzexia <runzexia@yunify.com>
2019-12-27 16:20:28 +0800

    update package name
    Signed-off-by: runzexia <runzexia@yunify.com>

---
+++ department-service/src/main/java/io/kubesphere/services/department/client/EmployeeClient.java
@@ -0,0 +1,17 @@
+package io.kubesphere.services.department.client;
+
+import java.util.List;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import io.kubesphere.services.department.model.Employee;
+
+@FeignClient(name = "employee-service")
+public interface EmployeeClient {
+
+	@GetMapping("/department/{departmentId}")
+	List<Employee> findByDepartment(@PathVariable("departmentId") Long departmentId);
+	
+}
---
+++ organization-service/src/main/java/io/kubesphere/services/organization/client/DepartmentClient.java
@@ -0,0 +1,19 @@
+package io.kubesphere.services.organization.client;
+
+import java.util.List;
+
+import io.kubesphere.services.organization.model.Department;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient(name = "department-service")
+public interface DepartmentClient {
+
+	@GetMapping("/organization/{organizationId}")
+	public List<Department> findByOrganization(@PathVariable("organizationId") Long organizationId);
+	
+	@GetMapping("/organization/{organizationId}/with-employees")
+	public List<Department> findByOrganizationWithEmployees(@PathVariable("organizationId") Long organizationId);
+	
+}
